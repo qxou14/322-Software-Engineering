4f473df878630df5a48e0e975ceefdee589d7085
QianXing Ou <61768334+qxou14@users.noreply.github.com>
2020-11-15 01:31:08 -0500

    Add files via upload

--- sourceCode/afterlogin.php
+++ sourceCode/afterlogin.php
@@ -13,6 +13,7 @@
     <span><a href="index.php"> Home </a></span>
     <span><a href = "AboutUs.html">About Us</a></span>
     <span><a href = "Contact.html">Contact</a></span>
+    <span><a href = "deposit.php"> Deposit</a></span>
     <span><a href = "logout.php"> Log out</a></span>
 </div>
 
@@ -25,8 +26,12 @@
 
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    //$row['saving']
     echo "<h3><i> Money left: {$row['saving']} </i></h3>";
 
+    $sql = "SELECT warning FROM info  WHERE username = '$username' ";
+    $result = $conn->query($sql);
+    $row = $result ->fetch_assoc();
+    echo "<h3><i> Warnings: {$row['warning']} </i></h3>";
 
 ?>
+
