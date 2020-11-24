a7ab060a4aa9306b5f5a686e7cda962d1b5d5114
AMaharj <47724128+AMaharj@users.noreply.github.com>
2020-11-24 14:08:38 -0500

    Update afterlogin.php

--- sourceCode/afterlogin.php
+++ sourceCode/afterlogin.php
@@ -1,18 +1,9 @@
 <?php 
     include "sectionStart.php";
     include "database.php";
+    require"menu.php";
+
 ?>
-
-<link rel = "stylesheet" type = "text/css" href ="style.css">
-
-<div class = "introduction"> The Online Restaurant </div>
-<div class = "look">
-    <span><a href="afterlogin.php"> Order </a></span>
-    <span><a href = "">Complain</a></span>
-    <span><a href = "deposit.php"> Deposit</a></span>
-    <span><a href = "cancelAccount.php"> Cancellation</a></span>
-    <span><a href = "logout.php"> Log out</a></span>
-</div>
 
 
 <h3><i>Welcome User: <?php echo $_SESSION['username']; ?> <i></h3>
