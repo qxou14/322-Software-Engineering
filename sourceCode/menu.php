f7aabc1ec95adcf120e5040e094feed154166a46
AMaharj <47724128+AMaharj@users.noreply.github.com>
2020-11-24 14:09:24 -0500

    Add files via upload

---
+++ sourceCode/menu.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel = "stylesheet" type = "text/css" href ="style.css">
+<link rel = "stylesheet" type = "text/css" href ="menustyle.css">
+
+<div class = "introduction"> The Online Restaurant </div>
+<div class = "look">
+    <span><a href="afterlogin.php"> Order </a></span>
+    <span><a href = "">Complain</a></span>
+    <span><a href = "deposit.php"> Deposit</a></span>
+    <span><a href = "cancelAccount.php"> Cancellation</a></span>
+    <span><a href = "logout.php"> Log out</a></span>
+</div>>
+
+</head>
+<body>
+<section class="menu"><h2 class="menu_title">Our Menu</h2>
+                    <div class="menu_section Chef_1_Dishes"><h3>Popular Dishes</h3>
+                    <div class="menu_item Yoshinoya_beef_bowl">
+                        <h4 class="name">Yoshinoya beef bowl</h4>
+                        <span class="price">$10.99  <button type="submit"  name="Add">Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>
+                        <p class="description">Food description</p>
+                        
+                        <hr>
+                    </div>
+                
+                    <div class="menu_item Nigirizushi">
+                        <h4 class="name">Nigirizushi</h4>
+                        <span class="price">$10.99  <button>Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>							<p class="description">Food Description</p>
+                        <hr>
+                    </div>
+                    <div class="menu_item Tamagoyaki">
+                        <h4 class="name">Tamagoyaki</h4>
+                        <span class="price">$10.99  <button>Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>							<p class="description">Food Description</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="menu_section Chef_2_Dishes"><h3>Cultural Dishes</h3>
+                    <div class="menu_item Unadon">
+                        <h4 class="name">Unadon</h4>
+                        <span class="price">$10.99  <button>Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>							<p class="description">Dish Description</p>
+                        <hr>
+                    </div>
+                
+                    <div class="menu_item Aburi_zushi">
+                        <h4 class="name">Aburi zushi</h4>
+                        <span class="price">$10.99  <button>Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>							<p class="description">Dish Description</p>
+                        <hr>
+                    </div>
+                    <div class="menu_item japanese_dumpling">
+                        <h4 class="name">japanese dumpling</h4>
+                        <span class="price">$10.99  <button>Add to Order</button><input type="number" id="qty" name="qty"
+   min="0" max="20"></span>							<p class="description">Dish Description</p>
+                        <hr>
+                    </div>
+                </div></section>
+
+
+
+
+</body>
+</html>
+
+
