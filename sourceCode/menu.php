168a973c0b0ebee6e8838b89a8741e9350b02c67
QianXing Ou <61768334+qxou14@users.noreply.github.com>
2020-11-26 16:12:02 -0500

    Update menu.php

--- sourceCode/menu.php
+++ sourceCode/menu.php
@@ -3,7 +3,7 @@
    $servername = "localhost";
    $username = "root";
    $password = "";
-   $dbname = "populardishes";
+   $dbname = "logininfo";
    
    // Create connection
    $conn = mysqli_connect($servername,$username,$password,$dbname);
